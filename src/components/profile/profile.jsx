90183bb842fa79cdf9000a2a9a34413c591c3800
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2024-03-10 17:11:22 +0200

    stules-to-3-1-added

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -1,33 +1,31 @@
+import css from "./Profile.module.css";
 
-const Profile = ({ username, tag, location, avatar, stats}) => {
+const Profile = ({ username, tag, location, avatar, stats }) => {
   return (
-    <div>
-<div>
-<img
-src={avatar}
-alt="User avatar"
-/>
-<p>{username}</p>
-<p>{tag}</p>
-<p>{location}</p>
-</div>
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img src={avatar} alt="User avatar" className={css.avatar} />
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
 
-<ul>
-<li>
-<span>Followers :</span>
-<span> {stats.followers}</span>
-</li>
-<li>
-<span>Views :</span>
-<span> {stats.views}</span>
-</li>
-<li>
-<span>Likes :</span>
-<span> {stats.likes}</span>
-</li>
-</ul>
-</div>
-  )
-}
+      <ul className={css.stats}>
+        <li>
+          <span className={css.label}>Followers</span>
+          <span className={css.quantity}>{stats.followers}</span>
+        </li>
+        <li>
+          <span className={css.label}>Views</span>
+          <span className={css.quantity}>{stats.views}</span>
+        </li>
+        <li>
+          <span className={css.label}>Likes</span>
+          <span className={css.quantity}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
